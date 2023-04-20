3a66aa8f193d6038d1d6e4850ab4a5e57243eed5
Saniul <saniulmallik@techis.io>
2023-04-20 15:49:24 +0530

    reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
 
 function App() {
+  const reducer = (state,action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return {count : state.count + 1 , showText: state.showText, number: state.number}
+      case 'REVERSE':
+        return {count :state.count , showText: !state.showText, number: state.number }
+      case 'DECREMENTNUM':
+        return {count : state.count , showText : state.showText , number: state.number - 1}
+      
+      case 'RESET':
+        return { count : 0, showText : false , number : 0}
+
+      default:
+        return state
+    }
+
+  }
+  const [state, dispatch] = useReducer(reducer,{count: 0 , showText: false, number: 0 })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{state.count}</h1>
+      <button onClick={()=>{
+        dispatch({type : 'INCREMENT'});
+        dispatch({type : 'REVERSE'})
+      }}>click here</button>
+      {state.showText && <p>This is a text</p>}
+    <h1>{state.number}</h1>
+    <button onClick={()=> {
+      dispatch({type: 'DECREMENTNUM'})
+    }}>click</button>
+
+    <button onClick={() =>{
+      dispatch({type: 'RESET'})
+    }}>RESET</button>
     </div>
   );
 }
